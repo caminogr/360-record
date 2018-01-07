689a7bbfe91843140de00c58860ec3f0fa04d003
kaminora <kaminoura9207@gmail.com>
2018-01-07 23:24:29 +0900

    Upload image to cloud instead of local

--- routes/photos.js
+++ routes/photos.js
@@ -1,15 +1,30 @@
 var express = require('express');
+var Datauri = require('datauri');
+var path = require('path');
+var multer = require('multer');
+var cloudinary = require('cloudinary');
+
 var router = express.Router();
 
-var multer = require('multer');
-var upload = multer({ dest: './public/images/uploads/' });
+var upload = multer({storage: multer.memoryStorage()})
+var dUri = new Datauri();
 
-var cloudinaryUploader = require('../lib/cloudinaryUploader');
+/* GET photos listing. */
+router.get('/', function(req, res, next) {
+  res.render('photos/index');
+});
 
 router.post('/', upload.single('image_file'), function(req, res, next) {
-  let path = req.file.path;
+  dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
 
-  cloudinaryUploader.uploadImage(path);
+  cloudinary.uploader.upload(dUri.content)
+  .then(function(image){
+    res.render('index')
+  })
+  .catch(function(err){
+    if (err){ console.warn(err);}
+    return res.status(500);
+  });
 });
 
 module.exports = router;
