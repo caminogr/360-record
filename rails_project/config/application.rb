d1c09c81a4b798f2945d8c3909674ac85ddf5ab6
kaminora <kaminoura9207@gmail.com>
2018-01-08 22:40:02 +0900

    Introduce rspotirfy

--- rails_project/config/application.rb
+++ rails_project/config/application.rb
@@ -6,8 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module RailsProject
   class Application < Rails::Application
+    RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
