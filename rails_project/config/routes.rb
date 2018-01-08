63b2e147aad9fe3d0ced235f79dd1968043d57e2
kaminora <kaminoura9207@gmail.com>
2018-01-09 01:33:57 +0900

    Try to use devise and rspotify

--- rails_project/config/routes.rb
+++ rails_project/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'photos#index'
+  resources :photos, only: [:index, :create]
+  put 'musics', to: 'musics#play'
+
+  get 'callback', to: 'callbacks#spotify'
+  # get '/auth/spotify/callback', to: 'users#spotify'
 end
